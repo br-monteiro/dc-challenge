def1d835b26c66a9fa24802b177a2c4315bac575
br-monteiro <edson.monteiro@chaordicsystems.com>
2020-08-10 02:49:58 -0300

    feat: added functions to manage the cache server

---
+++ part-1/src/cache.py
@@ -0,0 +1,32 @@
+import redis
+import json
+
+_cache = None
+_expiration = 600000 # 1000 * 60 * 10 = 600000 | 10 minutes
+
+def get_cache():
+  """
+  Returns the connection with cache server
+  """
+  global _cache
+  if _cache:
+    return _cache
+  _cache = redis.Redis(host='127.0.0.1', port=6379)
+  return _cache
+
+def has_key(key):
+  """
+  Check if the key ixists in cache server
+  """
+  return bool(get_cache().get(key))
+
+def save_cache(key: str, value):
+  """
+  Save a value in cache server.
+  By default the value expires according the value of _expiration
+  """
+  if type(key) != str:
+    return
+
+  data = json.dumps(value)
+  return get_cache().set(key, data, px=_expiration)
