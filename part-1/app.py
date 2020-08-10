942a5f21acdbfee8466728e67b43d3916a747e2e
br-monteiro <edson.monteiro@chaordicsystems.com>
2020-08-09 22:21:43 -0300

    feat: add the entry point of application

---
+++ part-1/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from src.utils import gethash, sortdict, build_message
+from src.cache import has_key, save_cache
+
+app = Flask(__name__)
+
+@app.route('/products', methods=['POST'])
+def products():
+  body = request.get_json()
+  body_hash = gethash(sortdict(body))
+
+  if has_key(body_hash):
+    return build_message('error', 'repeated product', body), 403
+  else:
+    save_cache(body_hash, body)
+    return build_message('success', 'saved product'), 200
+
+if __name__ == '__main__':
+  app.run(debug=True)
